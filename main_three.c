2bde9d303681e6882008c6752c177060c4ed1ac6
yeanh16 <yeanh16@gmail.com>
2017-10-29 03:11:05 +0000

    Mostly done

--- main_three.c
+++ main_three.c
@@ -10,7 +10,6 @@
 #include <errno.h>
 #include "main_three.h"
 
-#define MAX_THREADS 5
 #define BUFLEN 9999
 
 typedef struct thread_control_block {
@@ -49,7 +48,7 @@ void error(char *msg){
 
 /*
  Alternative to write
- */
+*/
 void write_new(int sock, char *msg) {
  int len = strlen(msg);
  if (send(sock, msg, len, 0) == -1) {
@@ -58,7 +57,7 @@ void write_new(int sock, char *msg) {
 }
 
 int main(int argc, char *argv[]) {
-    int sockfd, newsockfd, portno, clilen;
+    int sockfd, newsockfd, portno;
     struct sockaddr_in6 serv_addr, cli_addr;
     if (argc < 2) {
         fprintf(stderr, "ERROR, no port provided\n");
@@ -81,9 +80,6 @@ int main(int argc, char *argv[]) {
         error("ERROR on binding");
     }
     listen(sockfd, 5);
-    clilen = sizeof (cli_addr);
-    struct sockaddr_in6 their_addr;
-    socklen_t size = sizeof (their_addr);
     while (1) {
 
         
@@ -105,18 +101,7 @@ int main(int argc, char *argv[]) {
             }
             
         }
-        
-	//int newsock = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-	//pthread_t thread;
-	//if (pthread_create(&thread, 0, handle, &newsock)) error("Error creating thread");
-        /*
-	if (newsock == -1) {
-            perror("accept");
-        } else {
-            printf("Got a connection\n");
-            handle(&newsock);
-        }
-	*/
+      
     }
 
     return 0;
@@ -124,8 +109,8 @@ int main(int argc, char *argv[]) {
 
 void *handle(void *param) {
     	
-    	int j, file_fd, buflen, len;
-	long i, ret;
+    	int file_fd, buflen, len;
+	long i, j, ret;
 	char * fstr;
 	static char buffer[BUFLEN+1];
 	thread_control_block_t * tcb_p = param;
@@ -134,45 +119,38 @@ void *handle(void *param) {
 	
 	//print request buffer
 	printf("Request:\r\n%s\n", buffer);
-	
-	/*
-	//check that the contents are within range for the buffer and add terminator to the end
-	if(ret > 0 && ret < BUFLEN)	
-		buffer[ret]=0;	
-	else buffer[0]=0;
-	printf("buffer after 0's:\r\n%s\r\nEND\r\n",buffer);
-	
-	//replace returns and newlines with '*'
-	for(i=0;i<ret;i++)	
-		if(buffer[i] == '\r' || buffer[i] == '\n')
-			buffer[i]='*';
-	printf("buffer after 0's:\r\n%s\r\nEND\r\n",buffer);
-	*/
-	
-	
+		
 	//add terminator to end of the request part of the buffer as we are only interested in this (and not the headers)
-	for(i=4;i<BUFLEN;i++) { 
-		if(buffer[i] == ' ') { 
-			buffer[i] = 0;
+	for(i=4;i<BUFLEN;i++) {
+		if(buffer[i] == '/'){
+			for(j=i;j<BUFLEN;j++){
+				printf("j:%ld",j);
+				if(buffer[j] == ' ') { 
+					buffer[j] = 0;
+					break;
+				}
+			}
 			break;
 		}
 	}
 	
 	//printf("buffer after 0's:\r\n%s\r\nEND\r\n",buffer);
 	
-	
 	//null url handling
 	if( !strncmp(&buffer[0],"GET /\0",6) || !strncmp(&buffer[0],"get /\0",6) ) 
-		(void)strcpy(buffer,"GET /index.html");
-	
+		(void)strcpy(buffer,"GET /index.html\0");
+	if( !strncmp(&buffer[0],"HEAD /\0",7) || !strncmp(&buffer[0],"head /\0",7) ) 
+		(void)strcpy(buffer,"HEAD /index.html\0");
+		
 	//GET requests
-	if( strncmp(&buffer[0],"GET /\0",6) || !strncmp(&buffer[0],"get /\0",6) ){ 
+	if( !strncmp(&buffer[0],"GET /",5) || !strncmp(&buffer[0],"get /",5) ){ 
+		printf("GET request recieved\n");
 		buflen=strlen(buffer);
 		fstr = (char *)0;
 		for(i=0;extensions[i].ext != 0;i++) {
 			len = strlen(extensions[i].ext);
 			if( !strncmp(&buffer[buflen-len], extensions[i].ext, len)) {
-				fstr =extensions[i].filetype;
+				fstr = extensions[i].filetype;
 				break;
 			}
 		}
@@ -183,7 +161,7 @@ void *handle(void *param) {
 
 		//send response
 		int length = get_file_size(file_fd);
-		(void)sprintf(buffer,"HTTP/1.0 200 OK\r\nContent-Type: %s\r\n\r\n", fstr);
+		(void)sprintf(buffer,"HTTP/1.1 200 OK\r\nContent-Type: %s\r\n\r\n", fstr);
 		(void)write(fd,buffer,strlen(buffer));
 	
 		//print response buffer
@@ -195,7 +173,7 @@ void *handle(void *param) {
 		}
 	}
 	//HEAD requests
-	else if (strncmp(&buffer[0], "HEAD", 4)){
+	else if ( !strncmp(&buffer[0], "HEAD /", 6) || !strncmp(&buffer[0], "head /", 6) ){
 		buflen = strlen(buffer);
 		fstr = (char *)0;
 		for(i=0;extensions[i].ext != 0;i++) {
@@ -207,16 +185,21 @@ void *handle(void *param) {
 		}
 		
 		//see if file is available
-		if(( file_fd = open(&buffer[5],O_RDONLY)) == -1) 
+		if(( file_fd = open(&buffer[6],O_RDONLY)) == -1) 
 			error("File not supported");
 
 		//send response
 		int length = get_file_size(file_fd);
-		(void)sprintf(buffer,"HTTP/1.0 200 OK\r\nContent-Type: %s\r\nContent Length: %d\r\n", fstr,length);
+		(void)sprintf(buffer,"HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent Length: %d\r\n", fstr,length);
 		(void)write(fd,buffer,strlen(buffer));
 	
 		//print response buffer
 		printf("Response:\r\n%s\n", buffer);
+	}
+	//not supported html request
+	else{
+		write_new(fd, "HTTP/1.1	501 Not Implemented\r\n");
+		printf("Recieved bad request or not implemented\n");
 	}
 	
 	//close connection
@@ -230,28 +213,4 @@ int get_file_size(int fd) {
 	if (fstat(fd, &stat_struct) == -1)
   		return (1);
  	return (int) stat_struct.st_size;
-}
-
-
-int parse(const char* line){
-    /* Find out where everything is */
-    const char *start_of_path = strchr(line, ' ') + 1;
-    const char *start_of_query = strchr(start_of_path, '?');
-    const char *end_of_query = strchr(start_of_query, ' ');
-
-    /* Get the right amount of memory */
-    char path[start_of_query - start_of_path];
-    char query[end_of_query - start_of_query];
-
-    /* Copy the strings into our memory */
-    strncpy(path, start_of_path,  start_of_query - start_of_path);
-    strncpy(query, start_of_query, end_of_query - start_of_query);
-
-    /* Null terminators (because strncpy does not provide them) */
-    path[sizeof(path)] = 0;
-    query[sizeof(query)] = 0;
-
-    /*Print */
-    printf("%s\n", query);
-    printf("%s\n", path);
 }
